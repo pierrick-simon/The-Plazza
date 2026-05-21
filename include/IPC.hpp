76d4239720edb24ef1b005f784321bbda75724bf
Pierrick <pierrick.simon@epitech.eu>
2026-05-21 16:27:01 +0200

    feat: implementation of IPC via socket

---
+++ include/IPC.hpp
@@ -0,0 +1,62 @@
+/*
+** EPITECH PROJECT, 2026
+** The-Plazza
+** File description:
+** IPC
+*/
+
+#ifndef IPC_HPP
+    #define IPC_HPP
+
+#include <string>
+#include <unistd.h>
+#include "Exception.hpp"
+
+namespace Plazza {
+    class IPC {
+        public:
+            IPC(int fd) : _fd(fd) {};
+            ~IPC()
+            {
+                if (_fd != -1)
+                    close(_fd);
+            };
+
+            template<typename T>
+            void send(const T msg)
+            {
+                if (write(_fd, &msg, sizeof(T)) < 0)
+                    throw SendException();
+            }
+
+            template<typename T>
+            T receive()
+            {
+                T buf = {};
+                if (read(_fd, &buf, sizeof(T)) <= 0)
+                    throw CloseException();
+                return buf;
+            }
+
+            class IPCException : public PlazzaException {
+                public:
+                    IPCException(std::string str)
+                        : PlazzaException("IPC Error: " + str) {};
+            };
+
+            class SendException : public IPCException {
+                public:
+                    SendException() : IPCException("Send Failed!") {};
+            };
+
+            class CloseException : public IPCException {
+                public:
+                    CloseException() : IPCException("Close Connection!") {};
+            };
+
+        private:
+            int _fd;
+    };
+}
+
+#endif
